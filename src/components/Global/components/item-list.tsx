76005ef2e0a4e1be774d0bb00f6f1b3533a33af5
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-06-12 15:49:20 -0500

    Fix playing back from full screen lists on home screen (#1249)

--- src/components/Global/components/item-list.tsx
+++ src/components/Global/components/item-list.tsx
@@ -5,20 +5,39 @@ import { RefreshControl } from 'react-native'
 import { LegendListRenderItemProps } from '@legendapp/list/react-native'
 import ItemRow from './item-row'
 import { Queue } from '@/src/services/types/queue-item'
+import Track from './Track'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { BaseStackParamList } from '@/src/screens/types'
 
 interface ItemListProps {
 	query: UseInfiniteQueryResult<BaseItemDto[], Error>
 	queue?: Queue
 }
 
-export default function ItemList({ query }: ItemListProps): React.JSX.Element {
-	const renderItem = ({ item, index }: LegendListRenderItemProps<BaseItemDto>) => (
-		<ItemRow
-			circular={item.Type === BaseItemKind.MusicArtist}
-			item={item}
-			testID={`${item.Type?.toLowerCase()}-item-${index}`}
-		/>
-	)
+export default function ItemList({ query, queue }: ItemListProps): React.JSX.Element {
+	const tracks = query.data?.filter(({ Type }) => Type === BaseItemKind.Audio) ?? []
+
+	const navigation = useNavigation<NativeStackNavigationProp<BaseStackParamList>>()
+
+	const renderItem = ({ item, index }: LegendListRenderItemProps<BaseItemDto>) =>
+		item.Type === BaseItemKind.Audio ? (
+			<Track
+				navigation={navigation}
+				showArtwork
+				index={0}
+				track={item}
+				testID={`track-item-${index}`}
+				tracklist={tracks.slice(tracks.indexOf(item), tracks.indexOf(item) + 50)}
+				queue={queue ?? 'Library'}
+			/>
+		) : (
+			<ItemRow
+				circular={item.Type === BaseItemKind.MusicArtist}
+				item={item}
+				testID={`${item.Type?.toLowerCase()}-item-${index}`}
+			/>
+		)
 
 	const onEndReached = () => query.hasNextPage && query.fetchNextPage()
 
